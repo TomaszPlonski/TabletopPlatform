43bfc6b3a667f72f17b021175c3e1b2dd661b923
Tomasz Plonsk <tplonski@luxoft.com>
2022-12-22 14:45:50 +0100

    first commit

---
+++ UsersMS/src/main/java/com/tomaszplonski/TabletopPlatform/UsersMS/UsersMsApplication.java
@@ -0,0 +1,14 @@
+package com.tomaszplonski.TabletopPlatform.UsersMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersMsApplication.class, args);
+		System.out.printf("Hello world");
+	}
+
+}
---
+++ UsersMS/src/test/java/com/tomaszplonski/TabletopPlatform/UsersMS/UsersMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomaszplonski.TabletopPlatform.UsersMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
