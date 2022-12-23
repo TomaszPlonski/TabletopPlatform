12e488852a8c97a837d0490f504cd57c584c5b7e
Tomasz Plonski <tom.plonski@gmail.com>
2022-12-23 14:41:26 +0100

    added ApiGateway

---
+++ ApiGateway/src/test/java/com/tomaszplonski/TabletopPlatform/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomaszplonski.TabletopPlatform.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
